79b3d614a1d4ae741de27cf1dbcae188b39d31cb
Sara Gfroerer <sara.gfroerer@googlemail.com>
2019-11-26 16:36:20 +0100

    Design almost finished

--- src/DetailedSpot.js
+++ src/DetailedSpot.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import spotsData from './spots.json'
-import { func } from 'prop-types'
 import DetailedRoute from './DetailedRoute'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function DetailedSpot({ spot }) {
   return (
     <DetailedSpotStyled>
-      <img className="map-icon" src={require('../src/icons/map-white.svg')} />
+      <div className="map-icon">
+        <img src={require('../src/icons/map-white.svg')} />
+      </div>
       <Link className="arrow-icon" to="/">
         <img src={require('../src/icons/arrow-white.svg')} />
       </Link>
 
-      <img
-        className="heart-icon-white"
-        src={require('../src/icons/heart-white.svg')}
-      />
+      <div className="heart-icon">
+        <img src={require('../src/icons/heart-white.svg')} />
+      </div>
       <img src={spot.mainImage} className="mainImage"></img>
-      <h2>{spot.name}</h2>
+      <h2 className="spotName">{spot.name}</h2>
       <div className="routeContainer">
         {spot.routes.boulder.map(route => (
           <DetailedRoute
@@ -33,12 +32,13 @@ export default function DetailedSpot({ spot }) {
 
 const DetailedSpotStyled = styled.div`
   display: grid;
+  position: relative;
   height: 100vh;
   grid-template-rows: 400px auto;
+  gap: 10px;
 
   .mainImage {
     width: 100vw;
-
     height: 400px;
     object-fit: cover;
     border-bottom-left-radius: 24px;
@@ -46,28 +46,41 @@ const DetailedSpotStyled = styled.div`
   }
 
   .map-icon {
-    position: absolute;
     right: 10px;
     top: 14px;
-    width: 14px;
-    z-index: 2;
   }
-
-  .heart-icon-white {
-    position: absolute;
+  .heart-icon {
     right: 10px;
     top: 54px;
-    width: 14px;
-    fill: #fff;
-    z-index: 2;
+  }
+
+  .heart-icon,
+  .map-icon,
+  .arrow-icon {
+    position: absolute;
+    width: 35px;
+    height: 35px;
+    background: var(--gradientcolordark);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
   }
   .routeContainer {
     overflow: scroll;
   }
   .arrow-icon {
-    position: absolute;
-    top: 30vh;
+    top: 10px;
     left: 10px;
-    z-index: 2;
+  }
+
+  .spotName {
+    background: var(--gradientcolordark);
+    padding: 5px;
+    position: absolute;
+    top: 380px;
+    left: 20px;
+    border-radius: 4px;
+    color: var(--white);
   }
 `
