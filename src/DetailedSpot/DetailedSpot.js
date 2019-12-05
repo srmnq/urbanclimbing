8148b9dfb4bbbd5c2cfd65f6c2648454a1879cf8
Sara Gfroerer <sara.gfroerer@googlemail.com>
2019-12-05 15:56:08 +0100

    Removed need of secong State in detailed Spot

--- src/DetailedSpot/DetailedSpot.js
+++ src/DetailedSpot/DetailedSpot.js
@@ -1,74 +1,50 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import styled from 'styled-components'
 import RouteDescription from './RouteDescription'
 import { Link } from 'react-router-dom'
-import { getSpot } from '../services'
 
 export default function DetailedSpot({ toggleIsClimbed, spots }) {
   const pathname = window.location.pathname
   const id = pathname.substring(1)
 
-  const [spot, setSpot] = useState()
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    console.log(window.location.pathname)
-    getSpot(id).then(res => {
-      setSpot(res)
-      setLoading(false)
-    })
-  }, [])
-
-  // const index = spots.findIndex(el => el._id === id)
-  // const spot = spots[index]
+  const index = spots.findIndex(el => el._id === id)
+  const spot = spots[index]
   return (
-    <>
-      {loading ? (
-        <div>nein</div>
-      ) : (
-        <DetailedSpotStyled>
-          <Link className="map-icon" to="/map">
-            <div>
-              <img
-                alt="map-icon"
-                src={require('../../src/icons/map-white.svg')}
-              />
-            </div>
-          </Link>
-          <Link className="arrow-icon" to="/">
-            <img
-              alt="arrow-icon"
-              src={require('../../src/icons/arrow-white.svg')}
-            />
-          </Link>
+    <DetailedSpotStyled>
+      <Link className="map-icon" to="/map">
+        <div>
+          <img alt="map-icon" src={require('../../src/icons/map-white.svg')} />
+        </div>
+      </Link>
+      <Link className="arrow-icon" to="/">
+        <img
+          alt="arrow-icon"
+          src={require('../../src/icons/arrow-white.svg')}
+        />
+      </Link>
 
-          <div className="heart-icon">
-            <img
-              alt="heart-icon"
-              src={require('../../src/icons/heart-white.svg')}
-            />
-          </div>
-          <img
-            alt="climbing spot"
-            src={spot.mainImage}
-            className="mainImage"
-          ></img>
-          <h2 className="spotName">{spot.name}</h2>
-          <div className="routeContainer">
-            {spot.routes.boulder.map((route, index) => (
-              <RouteDescription
-                key={index}
-                routeName={route.routeName}
-                description={route.description}
-                difficulty={route.difficulty}
-                toggleIsClimbed={() => toggleIsClimbed(index, spot)}
-                isClimbed={route.isClimbed}
-              />
-            ))}
-          </div>
-        </DetailedSpotStyled>
-      )}
-    </>
+      <div className="heart-icon">
+        <img
+          alt="heart-icon"
+          src={require('../../src/icons/heart-white.svg')}
+        />
+      </div>
+      <img alt="climbing spot" src={spot.mainImage} className="mainImage"></img>
+      <h2 className="spotName">{spot.name}</h2>
+      <div className="routeContainer">
+        {spot.routes.boulder.map((route, index) => (
+          <RouteDescription
+            key={index}
+            routeName={route.routeName}
+            description={route.description}
+            difficulty={route.difficulty}
+            toggleIsClimbed={() => toggleIsClimbed(index, spot)}
+            isClimbed={route.isClimbed}
+          />
+        ))}
+      </div>
+    </DetailedSpotStyled>
   )
 }
 
